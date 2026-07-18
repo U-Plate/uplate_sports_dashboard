9319f0e4b4f44d29914b5c1015b56c3bb90c9065
TrackerJo <nkemme54@gmail.com>
2026-07-18 10:22:27 -0400

    Made available for deploying

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
+
+const root = resolve(__dirname, 'src')
+const outDir = resolve(__dirname, 'dist')
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root,
+  publicDir: resolve(__dirname, 'public'),
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+    },
+  },
+  base: '/',
+  build: {
+    outDir,
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        index: resolve(root, 'index.html'),
+
+      },
+    },
+  },
 })
